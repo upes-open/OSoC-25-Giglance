3d33d2465e4ef2b396624fce3a31b36e68b50884
Shubhojit [ControlShiftAI] <shubhojit@controlshiftai.com>
2025-07-01 18:33:11 +0530

    refactor: api url

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,11 +1,11 @@
-import { env } from '@/env';
-import axios from 'axios';
+import { env } from "@/env";
+import axios from "axios";
 
-const BASE_URL = env.NEXT_PUBLIC_SERVER_API_URL || 'http://localhost:5000/api';
+const BASE_URL = env.NEXT_PUBLIC_SERVER_API_URL || "http://localhost:5000";
 
 export const api = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: `${BASE_URL}/api`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
